78643f6ee65cc03b7aee36315012e0bc3a00d78c
djlee1987 <leedanjun@gmail.com>
2017-01-18 17:07:21 -0500

    hello world program in Python
    UIS - Data Science Essentials - Lab 2

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+'''
+Created on Jan 18, 2017
+
+@author: djlee1987 Daniel Lee
+Lab 2: Set up Git for Data Science Essentials (UIS)
+Program print 'hello world' and exits
+'''
+
+print('hello world')
